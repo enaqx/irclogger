944dad0b6c64c60fad534ef622618bf021db7490
Nick Raienko <enaqxx@gmail.com>
2014-10-23 05:22:42 +0300

    Add static streaming

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
+var http = require('http');
+var fs = require('fs');
+
 var irc  = require('irc');
 var config = require('./config');
 
+// Logging client
 var client = new irc.Client(
   config.irc_server,
   config.logger_nickname,
@@ -11,7 +15,26 @@ var client = new irc.Client(
   }
 );
 
+// Streaming messages to file
 var eventStr = 'message' + config.channel;
 client.addListener(eventStr, function (from, message) {
-    console.log('<%s> %s', from, message);
+  var timeStamp = new Date();
+  var strToLog = '[' + timeStamp.toLocaleTimeString() + '] ' +
+                 '<' + from + '>: ' + 
+                 message + '\n';
+  console.log(strToLog);
+  fs.appendFile(config.log_filename, strToLog, function (err) {
+    if (err) throw err;
+  });
 });
+
+// Streaming messages to webserver
+http.createServer(function (req, res) {
+  var logStream = fs.createReadStream(config.log_filename);
+  logStream.on('open', function () {
+    logStream.pipe(res);
+  });
+  logStream.on('error', function(err) {
+    res.end(err);
+  });
+}).listen(8080);
--- config.js
+++ config.js
@@ -5,5 +5,7 @@ var config = {};
 config.irc_server = 'irc.freenode.net';
 config.logger_nickname = 'irclogger';
 config.channel = '#testchan';
+config.max_messages = 10000;
+config.log_filename = 'log.txt'
 
 module.exports = config;
